da163c011c729d5f21a3dd80565ee72432b3eaf8
ejioforcelestine <ejioforcelestine77@gmail.com>
2024-02-01 01:28:19 +0100

    feat: added cloudinary to config

--- src/common/config/environment.js
+++ src/common/config/environment.js
@@ -1,21 +1,26 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const ENVIRONMENT = {
   APP: {
     NAME: process.env.APP_NAME,
     PORT: process.env.PORT || 3000,
-    ENV: process.env.APP_ENV,
+    ENV: process.env.APP_ENV
   },
   DB: {
-    URL: process.env.DB_URL,
+    URL: process.env.DB_URL
   },
   JWT: {
     ACCESS_KEY: process.env.ACCESS_JWT_KEY,
-    REFRESH_KEY: process.env.REFRESH_JWT_KEY,
+    REFRESH_KEY: process.env.REFRESH_JWT_KEY
   },
   JWT_EXPIRES_IN: {
     ACCESS: process.env.ACCESS_JWT_EXPIRES_IN,
-    REFRESH: process.env.REFRESH_JWT_EXPIRES_IN,
+    REFRESH: process.env.REFRESH_JWT_EXPIRES_IN
   },
+  CLOUDINARY: {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    API_KEY: process.env.CLOUDINARY_CLOUD_API_KEY,
+    API_SECRET: process.env.CLOUDINARY_CLOUD_API_SECRET
+  }
 };
